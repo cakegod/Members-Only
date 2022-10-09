7864789abf6856b9a1805e3c9840cc9fb3c7c499
Cake <cakemancerr@gmail.com>
2022-10-09 22:09:41 +0200

    Remove boilerplate

--- src/controllers/page.ts
+++
@@ -1,9 +0,0 @@
-import async from 'async';
-import { Request, Response, nextFunction } from 'express';
-import { body, validationResult } from 'express-validator';
-
-const controller = (req: Request, res: Response, next: nextFunction) => {
-	res.send('E.');
-};
-
-export default controller;
